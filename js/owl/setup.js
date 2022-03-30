a1479b3a3694011f1713917615772ffe3ff9ee21
João Vitor Carvalho <njvcarvalho@gmail.com>
2022-03-30 02:01:47 -0300

    Corpo do Site

---
+++ js/owl/setup.js
@@ -0,0 +1,29 @@
+center:true
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
